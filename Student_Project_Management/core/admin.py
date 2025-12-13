5402ce0bdf3ddab6cf87faf7aa8a0ed4b0b91665
Hanish <hanishdevaraj@gmail.com>
2025-12-13 09:24:47 +0530

    Admin.py is coded for users

--- Student_Project_Management/core/admin.py
+++ Student_Project_Management/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # show user_type in the list
+    list_display = ("username", "email", "first_name", "last_name", "user_type", "is_staff")
+    list_filter = ("user_type", "is_staff", "is_superuser", "is_active")
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Role information", {"fields": ("user_type",)}),
+    )
