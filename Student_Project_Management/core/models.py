77ecb293386d8b0c66f68fdac0b78a21943903ac
Hanish <hanishdevaraj@gmail.com>
2025-12-13 09:13:22 +0530

    Creation of initial models(Student,Faculty and HOD)

--- Student_Project_Management/core/models.py
+++ Student_Project_Management/core/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class UserType(models.TextChoices):
+        STUDENT = "STUDENT", "Student"
+        FACULTY = "FACULTY", "Faculty"
+        HOD = "HOD", "Head of Department"
+
+    user_type = models.CharField(
+        max_length=20,
+        choices=UserType.choices,
+        default=UserType.STUDENT,
+    )
+
+    # later we can add more fields like phone, etc.
+
+    def __str__(self):
+        return f"{self.username} ({self.user_type})"
