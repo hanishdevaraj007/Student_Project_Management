cb0bb39070d7f1015ac95dddda7b689862241c42
Hanish <hanishdevaraj@gmail.com>
2025-12-13 09:53:41 +0530

    MySql connection is made

--- Student_Project_Management/Student_Project_Management/settings.py
+++ Student_Project_Management/Student_Project_Management/settings.py
@@ -74,11 +74,19 @@ WSGI_APPLICATION = 'Student_Project_Management.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "student_project_db",
+        "USER": "fyp_user",
+        "PASSWORD": "StrongPassword123!",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 
 # Password validation
